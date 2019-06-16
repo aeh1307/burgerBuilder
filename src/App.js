3afcd5c4cc79d6644705ef11050dbf53cc4fc487
Astrid E Hamre <astridehamre@outlook.com>
2019-06-16 12:35:54 +0200

    layout comp. and startup bruger comp.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import Layout from './components/layout/Layout';
+import BurgerBuilder from './containers/burgerBuilder/BurgerBuilder';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+            <BurgerBuilder/>
+        </Layout>
+      </div>
+    );
+  }
+
 }
 
 export default App;

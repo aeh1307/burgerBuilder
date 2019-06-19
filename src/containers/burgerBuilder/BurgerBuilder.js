f576cb06111fa055ca3d0578eab7582079025688
Astrid Eidsaa Hamre <AstridEHamre@outlook.com>
2019-06-19 09:15:31 +0200

    Worked on add and remove methods for ingredients

--- src/containers/burgerBuilder/BurgerBuilder.js
+++ src/containers/burgerBuilder/BurgerBuilder.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/burger/Burger';
+import BuildControls from '../../components/burger/buildControls/BuildControls';
 
+
+const INGREDIENDT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
 
 class BurgerBuilder extends Component{
     // constructor(props){
@@ -17,13 +25,28 @@ class BurgerBuilder extends Component{
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+addIngredientHandler = (type) =>{
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount +1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount; 
+    const priceAddition = INGREDIENDT_PRICES[type];
+}
+
+
+
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         );
     }
